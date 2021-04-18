62771612e57a3be9fcf64da53289b8f9c9205643
johannbed <79025158+johannbed@users.noreply.github.com>
2021-04-18 09:59:26 -0500

    actua

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var menuToggle = document.querySelector(".menu-toggle");
+var menu = document.getElementById("menu");
+menuToggle.addEventListener("click", (e) => {
+    menu.classList.toggle("mostrar");
+})
